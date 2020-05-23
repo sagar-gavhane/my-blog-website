a472fea10ace050d31cabe829fd0806091194b55
sagar <sgavhane70@gmail.com>
2020-05-23 20:32:35 +0530

    remove bold text styles of name

--- components/Text/styles.js
+++ components/Text/styles.js
@@ -3,14 +3,4 @@ import styled from 'styled-components'
 export const StyledText = styled.p`
   color: #1e1e3f;
   margin: 1rem 0;
-
-  strong {
-    color: #4d21fc;
-    margin: 1rem 0;
-    font-weight: bold;
-  }
-
-  a:focus {
-    outline: none;
-  }
 `
